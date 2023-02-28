bba06120eff51b9e1f1f3ee20decfbe318cb2cb5
Dimon322228322 <lazdimasik@gmail.com>
2023-02-28 18:57:17 +0300

    2025 finished - omg i finally figure out equalations for graph

--- timus_2025/main.cpp
+++ timus_2025/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main(){
-
+    int numTest, n, k;
+    cin >> numTest;
+    for (int i = 0; i < numTest; ++i){
+        cin >> n >> k;
+        int comGroup = n / k, sum = 0, rest = n % k;
+        sum += k * (k - 1) / 2 * pow(comGroup,2);
+        for ( int j = 0; j < rest; ++j ) sum += comGroup * (k - 1) + j;
+        cout << sum << "\n";
+    }
+    return 0;
 }
