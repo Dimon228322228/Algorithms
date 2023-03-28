1d463adbfe3059ca6e32d2b5e2e5a4fa268a0390
Dimon322228322 <lazdimasik@gmail.com>
2023-03-28 22:02:31 +0300

    Complete F task

--- F/main.cpp
+++ F/main.cpp
@@ -5,12 +5,9 @@
 using namespace std;
 
 int main(){
-    vector<string> s(100);
+    vector<string> s;
     string n;
-    while (cin >> n){
-        if ( n.empty() ) break;
-        s.push_back(n);
-    }
+    while (cin >> n) s.push_back(n);
     sort( s.begin(), s.end(), []( const string& s1, const string& s2 ){return s1 + s2 > s2 + s1;});
     for (const auto & i : s) cout << i;
     return 0;
