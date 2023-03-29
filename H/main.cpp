c513c507225e8210f32581eef703656c47468422
Dimon322228322 <lazdimasik@gmail.com>
2023-03-29 09:41:36 +0300

    Finish H task

--- H/main.cpp
+++ H/main.cpp
@@ -8,24 +8,12 @@ int main(){
     int n, k, payment = 0;
     cin >> n >> k;
     vector<int> nums(n);
-    for (int i = 0; i < n; i++) cin >> nums[i];
-
-    sort(nums.rbegin(), nums.rend());
-    for (int i = 0; i < n; i++) cout << i << ": " << nums[i] << "\t";
-
-    for (int i = 0; i < (n / k) * k; i += k)
-        for (int j = 0; j < k - 1; j++) {
-            payment += nums[i + j];
-            cout << nums[i+j] << "  " << i + j << "\n";
-            cout << i << " " << j << "\n";
-        }
-
-    for (int i = (n / k) * k; i < n; i++) {
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
         payment += nums[i];
-        cout << nums[i] << "  " << i<< "\n";
-        cout << i << " " << "\n";
     }
-
+    sort(nums.rbegin(), nums.rend());
+    for (int i = k - 1; i < n; i+=k) payment -= nums[i];
     cout << payment;
     return 0;
 }
