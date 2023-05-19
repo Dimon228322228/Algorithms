3b04301a1ce0dc061eeebc47b815bbfed0a62461
Dimon322228322 <lazdimasik@gmail.com>
2023-05-20 00:54:47 +0300

    Task M

--- M/main.cpp
+++ M/main.cpp
@@ -5,28 +5,21 @@
 using namespace std;
 
 struct Cell {
-    int x, y, time;
-    string path;
-    Cell(int x, int y, int time, string path) : x(x), y(y), time(time), path(path) {}
+    int coord, time, prev;
+    char dir;
+    Cell(int coord, int time, int prev, char dir) : coord(coord), time(time), prev(prev), dir(dir) {}
 };
 bool operator < (const Cell &c1, const Cell &c2){ return c1.time > c2.time; }
+bool isValid(int x, int y, int N, int M) { return (x >= 0 && x < N && y >= 0 && y < M); }
 
-bool isValid(int x, int y, int N, int M) {
-    return (x >= 0 && x < N && y >= 0 && y < M);
-}
-
-pair<int, string> findPath(vector<vector<char>>& map, int startX, int startY, int endX, int endY) {
+Cell findPath(vector<vector<char>>& map, vector<Cell>& visited, int startX, int startY, int endX, int endY) {
     int N = map.size(), M = map[0].size();
-
-    vector<vector<Cell>> visited(N, vector<Cell>(M, {-1, -1, -1, ""}));
-    vector<vector<int>> time(N, vector<int>(M, -1));
 
     priority_queue<Cell> q;
 
-    Cell startCell(startX, startY, 0, "");
+    Cell startCell(startX * M + startY, 0, -1, ' ');
     q.push(startCell);
-    visited[startX][startY] = startCell;
-    time[startX][startY] = 0;
+    visited[startX * M + startY] = startCell;
 
     int dx[] = {-1, 0, 1, 0};
     int dy[] = {0, 1, 0, -1};
@@ -36,10 +29,9 @@ pair<int, string> findPath(vector<vector<char>>& map, int startX, int startY, in
         Cell currCell = q.top();
         q.pop();
 
-        int currX = currCell.x;
-        int currY = currCell.y;
+        int currX = currCell.coord / M;
+        int currY = currCell.coord % M;
         int currTime = currCell.time;
-        string currPath = currCell.path;
 
         if (currX == endX && currY == endY) break;
 
@@ -49,29 +41,32 @@ pair<int, string> findPath(vector<vector<char>>& map, int startX, int startY, in
 
             if (isValid(newX, newY, N, M) && map[newX][newY] != '#') {
                 int newTime = currTime + (map[newX][newY] == 'W' ? 2 : 1);
-                if ( visited[newX][newY].time == -1 || visited[newX][newY].time > newTime ){
-                    string newPath = currPath;
-                    newPath += directions[i];
-                    Cell newCell(newX, newY, newTime, newPath);
+                if ( visited[newX * M + newY].time == -1 || visited[newX * M + newY].time > newTime ){
+                    Cell newCell(newX * M + newY, newTime, currCell.coord, directions[i]);
                     q.push(newCell);
-                    visited[newX][newY] = newCell;
-                    time[newX][newY] = newTime;
+                    visited[newX * M + newY] = newCell;
                 }
             }
         }
     }
-    return make_pair(visited[endX][endY].time, visited[endX][endY].path);
+    return visited[endX * M + endY];
 }
 
 int main() {
     size_t N, M; int startX, startY, endX, endY;
     cin >> N >> M >> startX >> startY >> endX >> endY;
     vector<vector<char>> map(N, vector<char>(M));
+    vector<Cell> visited(N * M, {-1, -1, -1, ' '});
     for (int i = 0; i < N; i++)
         for (int j = 0; j < M; j++)
             cin >> map[i][j];
-    pair<int, string> result = findPath(map, startX - 1, startY - 1, endX - 1, endY - 1);
-    cout << result.first << endl;
-    cout << result.second << endl;
+    Cell result = findPath(map, visited, startX - 1, startY - 1, endX - 1, endY - 1);
+    cout << result.time << endl;
+    string path;
+    while ( result.prev != -1 ){
+        path = result.dir + path;
+        result = visited[result.prev];
+    }
+    cout << path << endl;
     return 0;
 }
